72e86f1b9a0f1d02d675679cbf5d6f43319096cf
Ylan <ylan_vinicio@hotmail.com>
2023-04-17 18:13:44 -0300

    Aula + Console.log

---
+++ Console.log/exercicio.js
@@ -0,0 +1,1 @@
+console.log(`Meu nome é "Ylan". Estou aprendendo JavaScript às`, 18, `da noite.`)
---
+++ Console.log/index.js
@@ -0,0 +1,9 @@
+//* String = Texto
+
+console.log('Ylan');
+console.log("Ylan");
+console.log(`Ylan`);
+
+//* Number = Numero
+
+console.log(123456);
