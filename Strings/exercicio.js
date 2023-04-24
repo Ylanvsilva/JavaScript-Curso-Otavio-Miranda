b6c3b9cbcae29a9ef5b2985b68e923083aaa394a
Ylan <ylan_vinicio@hotmail.com>
2023-04-24 18:12:28 -0300

    Exercicio de strings

---
+++ Strings/exercicio.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite seu nome completo: ');
+document.body.innerHTML += `O seu nome é ${nome}. <br />`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras. <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(1)}<br />`;
+document.body.innerHTML += `Qual o primeiro indice da letra A no seu nome? ${nome.indexOf('a')} <br />`;
+document.body.innerHTML += `Qual o ultimo indice da A letra no seu nome? ${nome.lastIndexOf('a')}<br />`;
+document.body.innerHTML += `As ultimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br />`;
+document.body.innerHTML += `Seu nome com letras maisculas:  ${nome.toUpperCase()}<br />`;
+document.body.innerHTML += `Seu nome com letras minusculas:  ${nome.toLowerCase()}<br />`;
