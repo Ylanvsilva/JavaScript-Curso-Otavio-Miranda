081af8931c6aa0337d95b2f5302a58677cbd2a64
Ylan <ylan_vinicio@hotmail.com>
2023-04-28 20:30:49 -0300

    Exercicio finalizado + Aula de operadores de comparação

--- Exercicio de função, array e objetos/script.js
+++ Exercicio de função, array e objetos/script.js
@@ -9,10 +9,38 @@ function meuEscopo () {
     //* Seleciona elementos como se estivesse selecionando classes, id, nome da tag, atributos e outros com CSS
     const form = document.querySelector('form'); //* 1° -> Selecionar o formulario (form)
 
-    //* Pegando um evento
-    form.onsubmit = function (evento) { //* Na função anonima colocasse um evento automatico (evento)
-        
-    }; //* Quando o formulario for enviado o evento de atualizar a pagina e previnido
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    // //* Pegando um evento
+    // form.onsubmit = function (evento) { //* Na função anonima colocasse um evento automatico (evento)
+    //     evento.preventDefault(); //* Navegador previne esse evento que era pra ocorrer por padrao, nao enviar o formulario
+    // }; //* Quando o formulario for enviado o evento de atualizar a pagina e previnido
+
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        //* Assim que esse formulario for enviado os valores e resultados serao passados para o objeto abaixo
+
+        pessoas.push({
+            nome: nome.value,   //* Value para pegar o valor dentro da variavel
+            sobrenome: sobrenome.value, 
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    }
+
+    form.addEventListener('submit', recebeEventoForm); //* Adiciona um escutador de eventos no meu formulario
 }
 
 meuEscopo();
---
+++ Operadores de comparação/index.js
@@ -0,0 +1,12 @@
+/**
+ * * Operadores de comparação -> comparam dois valores
+ * ? > - maior que
+ * ? >= - maior que ou igual a
+ * ? < - menor que
+ * ? <= - menor que ou igual a
+ * ? = - atribuição
+ * ? == - igualdade (valor) //! NAO USAR
+ * ? === - igualdade restrita (valor e tipo)
+ * ? != - diferente (valor) //! NAO USAR
+ * ? !== - diferente restrito (valor e tipo)
+ */
