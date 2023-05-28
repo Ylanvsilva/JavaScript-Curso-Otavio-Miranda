8aafad2abb1a345deffed1715f7f394ec460feaa
Ylan <ylan_vinicio@hotmail.com>
2023-05-27 21:27:48 -0300

    Object property e properties

--- Object.defineProperty() e Object.defineProperties()/index.js
+++ Object.defineProperty() e Object.defineProperties()/index.js
@@ -2,15 +2,13 @@
 //* defineProperties -> define varias de uma vez
 
 function Produto(nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
-    this.estoque = estoque;
-
     Object.defineProperty(this, 'estoque', {
         //* Estoque ser exibido (mostra a chave)
         enumerable: true,
         //* parametro estoque recebe valor
-        value: estoque,
+        value: function() {
+            return estoque;   
+        },
         //* Posso alterar o valor da propriedade estoque?
         writable: false,
         //* Posso configurar o valor da propriedade estoque?
@@ -19,9 +17,34 @@ function Produto(nome, preco, estoque) {
         //* Pode reconfigurar a chave?
         configurable: false
     });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: false,
+            configurable: false
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: false,
+            configurable: false    
+        },
+    })
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
 p1.estoque = 500000;
+//* Deletar a chave estoque
+delete p1.estoque;
 //* Variavel publica -> posso acessar ela fora do meu objeto
 console.log(p1.nome);
+//* Estoque se tornando função
+console.log(p1.estoque());
+//* Mostrar as chave em array
+console.log(Object.keys(p1));
+//* Iterar sobre as chaves do objeto
+for (let chave in p1) {
+    console.log(chave);
+}
